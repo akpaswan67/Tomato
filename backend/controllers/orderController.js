dd9c100381aeebf77e7cecd897feb8c7955c4788
Arun Kumar Paswan <arunkumarpaswn38@gmail.com>
2024-08-27 22:18:58 +0530

    final commit: fix bug

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -9,7 +9,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 const placeOrder = async (req,res) =>{
 
 	//when we deployed the online then frontend URL paste here.
-	const frontend_url = "http://localhost:5174"
+	const frontend_url = "http://localhost:5173"
 
 	try {
 		const newOrder = new orderModel({
