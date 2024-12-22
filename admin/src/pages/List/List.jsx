81acf4472a72df6b197b36a24411cba7cbf2b01a
ARUN KUMAR PASWAN <106347784+akpaswan67@users.noreply.github.com>
2024-12-22 19:36:41 +0530

    Update List.jsx

--- admin/src/pages/List/List.jsx
+++ admin/src/pages/List/List.jsx
@@ -7,7 +7,7 @@ const List = ({url}) => {
   const [list,setList] = useState([]);
 
   const fetchList = async () =>{
-    const response = await axios.get(`${url}/api/food/list`);
+    const response = await axios.get(`₹{url}/api/food/list`);
     
     if(response.data.success){
       setList(response.data.data);
@@ -18,7 +18,7 @@ const List = ({url}) => {
   }
 
   const removeFood = async (foodId) =>{
-    const response = await axios.post(`${url}/api/food/remove`,{id:foodId});
+    const response = await axios.post(`₹{url}/api/food/remove`,{id:foodId});
     await fetchList();
     if(response.data.success){
       toast.success(response.data.message);
@@ -47,7 +47,7 @@ const List = ({url}) => {
       {list.map((item,index)=>{
         return(
           <div key={index} className='list-table-format'>
-            <img src={`${url}/images/`+item.image} alt=""/>
+            <img src={`₹{url}/images/`+item.image} alt=""/>
             <p>{item.name}</p>
             <p>{item.category}</p>
             <p>₹{item.price}</p>
